79c66bc0fd6d424736bbced152ec3742ef9ecd28
Magdalena Grądek <magdalena.gradek@gmail.com>
2023-05-24 15:29:44 +0200

    pr03 start

--- project-no-3/pr03.js
+++ project-no-3/pr03.js
@@ -1,31 +1,6 @@
-const sizeUp = document.querySelector('.sizeUp');
-const sizeDown = document.querySelector('.sizeDown');
-const colorBtn = document.querySelector('.color');
-const p = document.querySelector('p');
-
-let fontSize = 36;
-
-const makeBigger = () => {
-    if (fontSize >= 70) return
-
-    fontSize += 5;
-    p.style.fontSize = fontSize + 'px';
-};
-
-const makeSmaller = () => {
-    if (fontSize <= 21) return
-    
-    fontSize -= 5;
-    p.style.fontSize = fontSize + 'px';
-};
-
-const changeColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    p.style.color = `rgb(${r},${g},${b})`
-}
-
-sizeDown.addEventListener('click', makeSmaller);
-sizeUp.addEventListener('click', makeBigger);
-colorBtn.addEventListener('click', changeColor)
+const pass = document.querySelector('#password');
+const p = document.querySelector('.passinfo');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minValue = 10;
